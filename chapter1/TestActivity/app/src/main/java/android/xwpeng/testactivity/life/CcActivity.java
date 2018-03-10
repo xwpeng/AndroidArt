cc590f8cc8f5334031ba6fc1f1b0a173ad55b4a2
xwpeng <cocpl0101001@gmail.com>
2018-03-10 18:20:36 +0800

    删除TestActivity子仓库

---
+++ chapter1/TestActivity/app/src/main/java/android/xwpeng/testactivity/life/CcActivity.java
@@ -0,0 +1,16 @@
+package android.xwpeng.testactivity.life;
+
+import android.os.Bundle;
+import android.xwpeng.testactivity.R;
+
+/**
+ * 非常普通的Activity
+ * Created by xwpeng on 16-8-24.
+ */
+public class CcActivity extends BasicActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cc);
+    }
+}
